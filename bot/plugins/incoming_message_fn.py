764ed52ce8e1bbe4a05e1ad69e77656a1f00b944
animepahe4988 <jatin61210@gmail.com>
2024-09-29 13:27:04 +0530

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -36,7 +36,7 @@ from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, Usern
 #from bot.helper_funcs.utils import(
 #  delete_downloads
 #)
-os.system("wget https://envs.sh/wyy.jpg")
+os.system("wget https://envs.sh/wyy.jpg -O thumb.jpg")
 
 #LOGS_CHANNEL = -1001283278354
 CURRENT_PROCESSES = {}
