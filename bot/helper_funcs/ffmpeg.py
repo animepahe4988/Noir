92fd9b73684b163e8da2272e38b474671c3f7649
animepahe4988 <jatin61210@gmail.com>
2024-10-12 17:33:50 +0530

    Update ffmpeg.py

--- bot/helper_funcs/ffmpeg.py
+++ bot/helper_funcs/ffmpeg.py
@@ -86,6 +86,7 @@ async def convert_video(video_file, output_directory, total_time, bot, message, 
     resolution.append("854x480")
     preset.append("veryfast")
     audio_b.append("40k")
+    watermark.append('-vf "drawtext=fontfile=font.ttf:fontsize=15:fontcolor=white:bordercolor=black@0.50:x=w-tw-10:y=10:box=1:boxcolor=black@0.5:boxborderw=6:text=Anime Sensei"')
     file_genertor_command = f"ffmpeg -hide_banner -loglevel quiet -progress '{progress}' -i '{video_file}' -metadata 'title=Encoded by Anime Bloodline' -c:v {codec[0]}  -map 0 -crf {crf[0]} -c:s copy -pix_fmt yuv420p -s {resolution[0]} -b:v 150k -c:a libopus -b:a {audio_b[0]} -preset {preset[0]} -metadata:s:v 'title=Anime Bloodline' -metadata:s:a 'title=Anime Bloodline' -metadata:s:s 'title=Anime Bloodline' '{out_put_file_name}' -y"
  #Done !!
     COMPRESSION_START_TIME = time.time()
